ef3937d9e4f0f0a4b2facf2d097ee61c95c12458
Andres Reyes <areyes.oleon@gmail.com>
2017-09-25 12:12:56 -0600

    Se creo el controlador para las busquedas de proyectos

--- controllers/project.js
+++ controllers/project.js
@@ -30,6 +30,32 @@ function save(req, res) {
    });
 }
 
+function search(req, res) {
+   Project.find({}).exec((err, ok) => {
+      if (err) {
+         res.status(500).send({
+            type: 'error',
+            message: 'Error en el servidor'
+         });
+      }
+      else {
+         if (ok) {
+            res.status(200).send({
+               type: 'success',
+               doc: ok
+            });
+         }
+         else {
+            res.status(404).send({
+               type: 'warning',
+               message: 'No hay información de proyectos.'
+            });
+         }
+      }
+   });
+}
+
 module.exports = {
-   save
+   save,
+   search
 }
