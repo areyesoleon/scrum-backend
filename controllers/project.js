20eb2177a1dc260d4fbfeb81cde99ad58dd96a3e
Andres Reyes <areyes.oleon@gmail.com>
2017-09-25 12:25:35 -0600

    Se creo controlador para buscar proyecto por ID

--- controllers/project.js
+++ controllers/project.js
@@ -55,7 +55,34 @@ function search(req, res) {
    });
 }
 
+function searchBy(req, res) {
+   let id = req.params.id;
+   Project.findById(id).exec((err, ok) => {
+      if (err) {
+         res.status(500).send({
+            type: 'error',
+            message: 'Error en el servidor.'
+         });
+      }
+      else {
+         if (ok) {
+            res.status(200).send({
+               type: 'success',
+               doc: ok
+            });
+         }
+         else {
+            res.status(404).send({
+               type: 'warning',
+               message: 'El proyecto que busca no existe.'
+            });
+         }
+      }
+   });
+}
+
 module.exports = {
    save,
-   search
+   search,
+   searchBy
 }
