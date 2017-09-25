f5b0f1ebb893c6dd9407537cb15fc048c8e3c10f
areyes <areyes.oleon@gmail.com>
2017-09-24 21:16:04 -0600

    Se creo el controlador del modulo de proyecto

---
+++ controllers/project.js
@@ -0,0 +1,31 @@
+'use strict'
+const Project = require('../models/project');
+
+function saveProject(req, res) {
+   let project = new Project();
+   let params = req.body;
+   project.name = params.name;
+   project.save((err, ok) => {
+      if (err) {
+         res.status(500).send({
+            type: 'error',
+            message: 'Error en el servidor'
+         });
+      }
+      else {
+         if (ok) {
+            res.status(200).send({
+               type: 'success',
+               message: 'Poyecto creado correctamente',
+               doc: ok
+            });
+         }
+         else {
+            res.status(404).send({
+               type: 'warning',
+               message:'Ingrese los datos correctamente.'
+            })
+         }
+      }
+   })
+}
