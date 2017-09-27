5e926192e92d98f4ec6c9f6c9e23628b15e44173
Andres Reyes <areyes.oleon@gmail.com>
2017-09-27 17:45:33 -0600

    Se agrego ruta para buscar por id en los productbackloghistory

--- routes/productBackLogHistory.js
+++ routes/productBackLogHistory.js
@@ -5,4 +5,5 @@ const ProductBackLogHistoryController = require('../controllers/productBackLogHi
 const api = express.Router();
 api.post('/save', ProductBackLogHistoryController.save);
 api.get('/search/:productBackLog', ProductBackLogHistoryController.search);
+api.get('/searchBy/:id', ProductBackLogHistoryController.searchBy);
 module.exports = api;
