f8b962a342024fa3f8cc25e550dbaf5a089dabba
areyes <areyes.oleon@gmail.com>
2017-09-26 21:14:47 -0600

    Se agregaron las rutas de filtros para los productbacklog

--- routes/productBackLog.js
+++ routes/productBackLog.js
@@ -3,6 +3,8 @@ const express = require('express');
 const ProductBackLogController = require('../controllers/productBackLog');
 const api = express.Router();
 
-api.post('/save',ProductBackLogController.save);
+api.post('/save', ProductBackLogController.save);
+api.get('/search/:project', ProductBackLogController.search);
+api.get('/searchBy/:id', ProductBackLogController.searchBy);
 
 module.exports = api;
