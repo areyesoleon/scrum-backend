6abfb4e415dacaeffd8ad222d061eb9f307a85df
Andres Reyes <areyes.oleon@gmail.com>
2017-09-27 18:01:13 -0600

    Se agrego la ruta para actualizar los productbackloghistory

--- routes/productBackLogHistory.js
+++ routes/productBackLogHistory.js
@@ -6,4 +6,5 @@ const api = express.Router();
 api.post('/save', ProductBackLogHistoryController.save);
 api.get('/search/:productBackLog', ProductBackLogHistoryController.search);
 api.get('/searchBy/:id', ProductBackLogHistoryController.searchBy);
+api.put('/update/:id', ProductBackLogHistoryController.update);
 module.exports = api;
