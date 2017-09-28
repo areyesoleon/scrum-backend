d4b736caeef8668330c7bd48221d0eabe118d216
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 10:51:20 -0600

    Se creo la ruta para poder eliminar sprint

--- routes/sprint.js
+++ routes/sprint.js
@@ -6,4 +6,5 @@ api.post('/save', SprintController.save);
 api.get('/search', SprintController.search);
 api.get('/searchBy/:id', SprintController.searchBy);
 api.put('/update/:id', SprintController.update);
+api.delete('/remove/:id', SprintController.remove);
 module.exports = api;
