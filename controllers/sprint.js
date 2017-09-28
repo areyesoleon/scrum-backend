cad0ac7b17dac10d8dc319cb19422c6a4bd6fe28
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 10:50:35 -0600

    se creo la funcion para eliminar sprint

--- controllers/sprint.js
+++ controllers/sprint.js
@@ -110,9 +110,34 @@ function update(req, res) {
       }
    });
 }
+function remove(req, res) {
+   let id = req.params.id;
+   Sprint.findByIdAndRemove(id).exec((err, ok) => {
+      if (err) {
+         res.status(500).json({
+            serverError
+         });
+      }
+      else {
+         if (ok) {
+            res.status(200).json({
+               type: 'success',
+               message: 'El sprint fue eliminado correctamente.'
+            });
+         }
+         else {
+            res.status(400).json({
+               type: 'error',
+               message: 'El sprint no pudo ser eliminado.'
+            });
+         }
+      }
+   });
+}
 module.exports = {
    save,
    search,
    searchBy,
-   update
+   update,
+   remove
 }
