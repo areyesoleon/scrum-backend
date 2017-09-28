6ff3c2f47c3f5c7f2a4b06b498a60e2e5b202de7
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 17:35:36 -0600

    Se agrego la ruta para actualizar roles

--- routes/roleUser.js
+++ routes/roleUser.js
@@ -2,5 +2,8 @@
 const express = require('express');
 const RoleUserController = require('../controllers/roleUser');
 const api = express.Router();
-api.post('/save', RoleUserController.save)
+api.post('/save', RoleUserController.save);
+api.get('/search', RoleUserController.search);
+api.get('/search/:id', RoleUserController.searchBy);
+api.put('/update/:id', RoleUserController.update);
 module.exports = api;
