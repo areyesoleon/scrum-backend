97a6135578940961f35579b4c372f944bde9f545
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 08:57:46 -0600

    Se agrego la ruta para buscar sprints

--- routes/sprint.js
+++ routes/sprint.js
@@ -3,4 +3,5 @@ const express = require('express');
 const SprintController = require('../controllers/sprint');
 const api = express.Router();
 api.post('/save',SprintController.save);
+api.get('/search',SprintController.search);
 module.exports = api;
