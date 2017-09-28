c7079ba8399936811c040fd05c15fc5dc6926752
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 16:37:14 -0600

    Se creo ruta para eliminar estados de tarea

--- routes/stateTask.js
+++ routes/stateTask.js
@@ -3,4 +3,8 @@ const express = require('express');
 const StateTaskController = require('../controllers/stateTask');
 const api = express.Router();
 api.post('/save', StateTaskController.save);
+api.get('/search', StateTaskController.search);
+api.get('/searchBy/:id', StateTaskController.searchBy);
+api.put('/update/:id', StateTaskController.update);
+api.delete('/remove/:id', StateTaskController.remove);
 module.exports = api;
