577b9c3aa338a892d6a6585e60c1d1a47f1d5ffb
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 12:37:59 -0600

    Se creo las rutas de buqueda y busqueda por id de los origenes de tareas

--- routes/originTask.js
+++ routes/originTask.js
@@ -2,5 +2,7 @@
 const express = require('express');
 const OriginTaskController = require('../controllers/originTask');
 const api = express.Router();
-api.post('/save',OriginTaskController.save);
+api.post('/save', OriginTaskController.save);
+api.get('/search', OriginTaskController.search);
+api.get('/searchBy/:id', OriginTaskController.searchBy);
 module.exports = api;
