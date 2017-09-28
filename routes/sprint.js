ad613d880a96dd737cbbcc39dcb7d16fa4c59132
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 10:41:21 -0600

    Se agrego la ruta para poder actualizar un sprint

--- routes/sprint.js
+++ routes/sprint.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const SprintController = require('../controllers/sprint');
 const api = express.Router();
-api.post('/save',SprintController.save);
-api.get('/search',SprintController.search);
-api.get('/searchBy/:id',SprintController.searchBy);
+api.post('/save', SprintController.save);
+api.get('/search', SprintController.search);
+api.get('/searchBy/:id', SprintController.searchBy);
+api.put('/update/:id', SprintController.update);
 module.exports = api;
