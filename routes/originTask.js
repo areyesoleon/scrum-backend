b851e39efa1b3993f4f5e82abfb897b37b745717
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 14:46:21 -0600

    Se agrego la ruta para poder eliminar origenes de tareas

--- routes/originTask.js
+++ routes/originTask.js
@@ -5,4 +5,6 @@ const api = express.Router();
 api.post('/save', OriginTaskController.save);
 api.get('/search', OriginTaskController.search);
 api.get('/searchBy/:id', OriginTaskController.searchBy);
+api.put('/update/:id', OriginTaskController.update);
+api.delete('/remove/:id', OriginTaskController.remove);
 module.exports = api;
