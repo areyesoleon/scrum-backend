ce465768ec2940ede7b200a7becb443e15bbd9f2
Andres Reyes <areyes.oleon@gmail.com>
2017-09-28 10:21:39 -0600

    Se creo la ruta para buscar sprint por id

--- routes/sprint.js
+++ routes/sprint.js
@@ -4,4 +4,5 @@ const SprintController = require('../controllers/sprint');
 const api = express.Router();
 api.post('/save',SprintController.save);
 api.get('/search',SprintController.search);
+api.get('/searchBy/:id',SprintController.searchBy);
 module.exports = api;
