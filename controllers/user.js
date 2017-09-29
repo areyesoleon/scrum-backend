2b5d2ac8c5f578e2a3e739a19f25adb057375c00
areyes <areyes.oleon@gmail.com>
2017-09-28 22:13:47 -0600

    Se creo la funcion para guardar usuarios

---
+++ controllers/user.js
@@ -0,0 +1,66 @@
+'use strict'
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const serverError = {
+   type: 'error',
+   message: 'Error en la consulta.'
+}
+function save(req, res) {
+   let user = new User();
+   let data = req.body;
+   user.name = data.name;
+   user.surName = data.surName;
+   user.email = data.email;
+   user.role = data.role;
+   if (data.password) {
+      bcrypt.hash(data.password, saltRounds, (err, hash) => {
+         if (err) {
+            res.status(500).json({
+               serverError
+            });
+         }
+         else {
+            user.password = hash;
+            user.save((err, ok) => {
+               if (err) {
+                  res.status(500).json({
+                     type: 'error',
+                     arrayError: err
+                  });
+               }
+               else {
+                  if (ok) {
+                     res.status(200).json({
+                        type: 'success',
+                        message: 'El usuario fue creado exitosamente.',
+                        doc:{
+                           name:ok.name,
+                           surName:ok.surName,
+                           email:ok.email,
+                           role:ok.role,
+                           password: '>:P desde aca no puedes verla.'
+                        } 
+                     });
+                  }
+                  else {
+                     res.status(400).json({
+                        type: 'warning',
+                        message: 'No se pudo crear el usuario.'
+                     });
+                  }
+               }
+            });
+         }
+      });
+   }
+   else {
+      res.status(404).json({
+         type: 'warning',
+         message: 'La contraseña no fue ingresada.'
+      })
+   }
+}
+module.exports = {
+   save
+}
