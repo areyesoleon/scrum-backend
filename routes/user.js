49a72f6bf2dd5398b18ec1dcd29e460e41cf20d4
areyes <areyes.oleon@gmail.com>
2017-10-01 00:10:03 -0600

    Se agregaron las rutas para eliminar usuarios;9C

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
 'use strict'
 const express = require('express');
-const UserController =  require('../controllers/user');
+const UserController = require('../controllers/user');
 const api = express.Router();
-api.post('/save',UserController.save);
-api.get('/search',UserController.search);
-api.get('/searchBy/:id',UserController.searchBy);
+api.post('/save', UserController.save);
+api.get('/search', UserController.search);
+api.get('/searchBy/:id', UserController.searchBy);
+api.put('/update/:id', UserController.update);
+api.delete('/remove/:id', UserController.remove);
+
 module.exports = api;
