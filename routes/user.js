fb1ae0654974471a5f83d1e0d90990ef61e6d781
areyes <areyes.oleon@gmail.com>
2017-09-30 23:47:32 -0600

    Se agrego las rutas para las busquedas

--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,6 @@ const express = require('express');
 const UserController =  require('../controllers/user');
 const api = express.Router();
 api.post('/save',UserController.save);
+api.get('/search',UserController.search);
+api.get('/searchBy/:id',UserController.searchBy);
 module.exports = api;
